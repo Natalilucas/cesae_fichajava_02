950b0f9fb3f9a39deef2ff5b0bc6bfe56f56ec2c
Natali Lucas <1222649@isep.ipp.pt>
2023-11-16 09:25:06 +0000

    Adicionado trabalho pratico

--- cesae_softdev_1023/FichasJava/src/TrabalhoPratico/TrabalhoPratico.java
+++ cesae_softdev_1023/FichasJava/src/TrabalhoPratico/TrabalhoPratico.java
@@ -1,9 +1,24 @@
 package TrabalhoPratico;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class TrabalhoPratico {
-    public static void main(String[] args) {
+
+    //Função para ler o ficheiro;
+    public static void lerFicheiro(String caminho) throws FileNotFoundException {
+        Scanner sc = new Scanner(new File(caminho));
+        String linha;
+
+        while(sc.hasNextLine()){
+            linha = sc.nextLine();
+            System.out.println(linha);
+        }
+    }
+
+
+    public static void main(String[] args) throws FileNotFoundException {
         Scanner input = new Scanner(System.in);
 
         //Definir o tipo, se ADMIN ou CLIENTE
@@ -26,7 +41,9 @@ public class TrabalhoPratico {
                     System.out.println("Se é administrador, digite sua senha: ");
                     digiteSenha = Integer.parseInt(input.next());
                     if(digiteSenha == password){
-                        System.out.println("Você entrou no programa!!");
+                        System.out.println("Bem Vindo ADMINISTRADOR!!\n");
+                        lerFicheiro("FicheiroTraPra/GameStart_V2.csv");
+
                     } else {
                         System.out.println("A senha está incorreta, digite novamente, ou digite N para sair do menu! ");
                         digiteSenha = Integer.parseInt(input.next());
